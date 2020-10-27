7294312b27b38ae09e086efe30a31ba6a190b92c
ryzen500 <72720477+ryzen500@users.noreply.github.com>
2020-10-27 14:23:14 +0700

    Add files via upload

---
+++ CRUD3/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        body{
+            background: radial-gradient(red,orange);
+        }
+        </style>
+</head>
+<body>
+<div class="judul">		
+		<h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
+		<h2>Menampilkan data dari database</h2>
+		<h3>By tsany </h3>
+        </div>
+        <hr>
+    <header><marquee width="100%">Welcome In SMK coding Surabaya </marquee></header>
+    <hr>
+    <h1 align="center" >Pendaftaran Siswa Baru Jurusan Coding</h1>
+    <h4>Menu</h4>
+    <nav>
+        <div class="body">
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-siswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+
+    </body>
+</html>
