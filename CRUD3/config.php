f3940392574ba335a78672c8b3ecec1ad33c26f2
ryzen500 <72720477+ryzen500@users.noreply.github.com>
2020-11-03 15:05:57 +0700

    Update config.php

--- CRUD3/config.php
+++ CRUD3/config.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$nama_database = "form_pendaftaran_siswa";
+$nama_database = "pendaftaran_siswa";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
