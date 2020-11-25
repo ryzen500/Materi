3ec228a26c86e9af465693f2a7ef34e8ced5880f
ameld <73698073+amel1210@users.noreply.github.com>
2020-11-25 08:40:44 +0700

    Add files via upload

---
+++ CRUD3/CRUD1/calon_siswa.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Nov 2020 pada 07.54
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pendaftaran_siswa`
+--
+
+-- --------------------------------------------------------
+--
+-- Struktur dari tabel `calon_siswa`
+--
+
+CREATE TABLE `pendaftaran_siswa`.`calon_siswa` (
+    `id` INT NOT NULL AUTO_INCREMENT ,  
+    `nama` VARCHAR(64) NOT NULL ,  
+    `alamat` VARCHAR(255) NOT NULL ,  
+    `jenis_kelamin` VARCHAR(16) NOT NULL ,  
+    `agama` VARCHAR(16) NOT NULL ,  
+    `sekolah_asal` VARCHAR(64) NOT NULL ,    
+    PRIMARY KEY  (`id`)
+) ENGINE = InnoDB;
+
+--
+-- Dumping data untuk tabel `calon_siswa`
+--
+
+INSERT INTO `calon_siswa` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`) VALUES
+(2, 'Mohammad habibie rafi', 'jalan teluk nibung nomer 34', 'laki-laki', 'Islam', 'smkn 2 surabaya'),
+(3, 'Achmad Tsany Wicaksono ', 'Jalan teluk nibung nomer 34', 'laki-laki', 'Islam', 'smkn 2 surabaya'),
+(4, 'Achmad Tsany Wicaksono ', 'Jalan teluk nibung nomer 34', 'laki-laki', 'Islam', 'smkn  5'),
+(5, 'VIKY FARISTIKA', 'sadadwada', 'laki-laki', 'Islam', 'dadwadsadw'),
+(6, 'bUAGAGGGAGAGA', 'pLANET BUMI', 'laki-laki', 'Atheis', 'dadwadsadw');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ CRUD3/CRUD1/index.php
@@ -0,0 +1,33 @@
+<head>
+<link rel="stylesheet" href="style1.css">
+<style ="text/css" 	background= url(0.jpg)></style>
+<title>Pendaftaran Siswa Baru | SMK Coding</title>
+<body> 
+    <div class="wrapper">
+        <header>
+          <ul>
+		    <h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
+		    <h2>Menampilkan data dari database</h2>
+            <h3>By tsany bego </h3>
+          </ul>
+        </header>
+        <hr color=red>
+    <marquee style=color:#fff width="100%" behavior="alternate"><b>Welcome In SMK coding Surabaya </b></marquee>
+        <hr color=red>
+<section class="courses">
+    <article>
+        <font color="darkred" </font>
+        <h2 align="left" >Pendaftaran Siswa Baru Jurusan Coding</h2>
+        <h3 align="left">Menu</h3>
+			<div style="border: 1px solid; padding: 1px; height: 65px; width: 200px; text-align: left"> 
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-siswa.php">Pendaftar</a></li>
+			<div>
+    </article>
+</section>
+<footer>
+    &copy; 2020
+</footer>
+</div><!-- .wrapper -->
+</body>
+</html>
